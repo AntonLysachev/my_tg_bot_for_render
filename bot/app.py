8dd822007621228704ff9f80089c2166d6853f63
AntonLysachev <anton@lysachev.ru>
2024-03-08 11:44:26 +0500

    Add dialigflow

--- bot/app.py
+++ bot/app.py
@@ -3,17 +3,35 @@ from flask import Flask, request
 import telebot
 from dotenv import load_dotenv
 import logging
+from google.cloud import dialogflow_v2
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 URL = os.getenv('URL')
 DEBUG_SWITCH = os.getenv('DEBUG_SWITCH')
+PROJECT_ID = os.getenv('PROJECT_ID')
+SESSION_ID = os.getenv('SESSION_ID')
 
 bot = telebot.TeleBot(TOKEN, threaded=False)
 app = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
+
+
+def get_dialogflow_response(user_input):
+    project_id = PROJECT_ID
+    session_id = SESSION_ID
+    language_code = 'ru'
+
+    session_client = dialogflow_v2.SessionsClient()
+    session_path = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow_v2.TextInput(text=user_input, language_code=language_code)
+    query_input = dialogflow_v2.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(session=session_path, query_input=query_input)
+    return response.query_result.fulfillment_text
 
 
 @bot.message_handler(commands=['start'])
@@ -23,7 +41,7 @@ def start(message):
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, get_dialogflow_response(message.text))
 
 
 @app.route(f'/{TOKEN}', methods=['POST'])
@@ -39,6 +57,7 @@ def index():
     bot.remove_webhook()
     bot.set_webhook(url=URL)
     return 'OK', 200
+
 
  
 if __name__ == "__main__":
